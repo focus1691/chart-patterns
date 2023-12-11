a08f32cc87990d0e2decf2531e7cb609130b9345
Joshua <joshua.ali@earzz.co.uk>
2023-12-11 23:03:26 +0000

    Add test

---
+++ test/ValueArea.test.ts
@@ -0,0 +1,8 @@
+import { ValueArea } from '../src/lib/ValueArea'
+
+describe('ValueArea', () => {
+  it('should be defined', () => {
+    const valueArea = new ValueArea()
+    expect(valueArea).toBeDefined()
+  })
+})
