d263e9967bf860e1116df5c8ab59d991e6bb5f98
Joshua <mrjoshuaali@yahoo.co.uk>
2024-01-15 00:30:22 +0000

    add date utils

---
+++ src/utils/date.ts
@@ -0,0 +1,44 @@
+import { ICandle } from 'src/types'
+import { ITradeWindow } from 'src/types/levels.types'
+
+export const isDST = (date: Date): boolean => {
+  // DST in the UK starts on the last Sunday in March and ends on the last Sunday in October
+  const march = new Date(date.getFullYear(), 2, 31)
+  const october = new Date(date.getFullYear(), 9, 31)
+
+  // Find last Sunday of March
+  const startDST = new Date(march.setDate(31 - march.getDay()))
+  // Find last Sunday of October
+  const endDST = new Date(october.setDate(31 - october.getDay()))
+
+  return date > startDST && date < endDST
+}
+
+export const isInitialBalanceFormed = (candle: ICandle): boolean => {
+  // Check if DST is in effect for openTime and closeTime
+  const dstAdjustment = isDST(candle.openTime) ? 60 * 60 * 1000 : 0
+
+  // Adjust openTime and closeTime for UTC+1 if DST is in effect
+  const adjustedOpenTime = new Date(candle.openTime.getTime() + dstAdjustment)
+  const adjustedCloseTime = new Date(candle.closeTime.getTime() + dstAdjustment)
+
+  // Extract hours and minutes from adjusted times
+  const openHour = adjustedOpenTime.getUTCHours()
+  const openMinutes = adjustedOpenTime.getUTCMinutes()
+  const closeHour = adjustedCloseTime.getUTCHours()
+  const closeMinutes = adjustedCloseTime.getUTCMinutes()
+
+  // Check if the candle is within the 00:30 - 01:00 UTC+1 interval
+  if (openHour === 0 && openMinutes >= 30 && closeHour === 0 && closeMinutes <= 59) {
+    return true
+  }
+
+  return false
+}
+
+export const isWithinTimeWindow = (window: ITradeWindow): boolean => {
+  const currentTime = new Date()
+  const currentHour = currentTime.getHours()
+
+  return currentHour >= window.start && currentHour < window.end
+}
