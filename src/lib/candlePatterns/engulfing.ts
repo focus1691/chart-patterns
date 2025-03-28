363f3efdc5c7544e8ac9ea42b4e1638f62801221
Joshua <mrjoshuaali@yahoo.co.uk>
2025-03-28 02:46:24 +0000

    update engulfing pattern

--- src/lib/candlePatterns/engulfing.ts
+++ src/lib/candlePatterns/engulfing.ts
@@ -10,34 +10,36 @@ import { IEngulfingCandleConfig } from '../../types/candlestickPatterns/engulfin
  *
  * @example
  * ```typescript
- * import { CandlestickPatterns } from 'chart-patterns';
+ * import * as ta from 'chart-patterns';
  *
- * // Assuming 'candles' is an array of candle data
- * const engulfingCandle: SIGNAL_DIRECTION | null = CandlestickPatterns.findEngulfingCandle({ candles, index: 10 });
+ * const engulfingCandle: SIGNAL_DIRECTION | null = ta.CandlestickPatterns.isEngulfingPattern({ candles, index: 10 });
  * ```
  */
-export const findEngulfingCandle = (config: IEngulfingCandleConfig): SIGNAL_DIRECTION | null => {
-  const candles: ICandle[] = config.candles;
+export const isEngulfingPattern = (config: IEngulfingCandleConfig): SIGNAL_DIRECTION => {
+  const { candles, index } = config;
   if (candles.length < 2) {
-    return null;
+    return SIGNAL_DIRECTION.NONE;
   }
 
-  const index = config.index ?? candles.length - 1;
-  if (!candles[index] || !candles[index - 1]) return null; // Ensures there's a previous candle
+  const startIndex = index ?? candles.length - 1;
+  if (!candles[startIndex] || !candles[startIndex - 1]) {
+    return SIGNAL_DIRECTION.NONE;
+  }
 
-  const prevCandle: ICandle = candles[index - 1];
-  const currCandle: ICandle = candles[index];
+  const prevCandle: ICandle = candles[startIndex - 1];
+  const currCandle: ICandle = candles[startIndex];
 
-  const isPreviousBullish: boolean = prevCandle.close > prevCandle.open;
-  const isPreviousBearish: boolean = prevCandle.close < prevCandle.open;
-  const isCurrentBullish: boolean = currCandle.close > currCandle.open;
-  const isCurrentBearish: boolean = currCandle.close < currCandle.open;
+  const prevBullish: boolean = prevCandle.close > prevCandle.open;
+  const prevBearish: boolean = prevCandle.close < prevCandle.open;
+  const currBullish: boolean = currCandle.close > currCandle.open;
+  const currBearish: boolean = currCandle.close < currCandle.open;
 
-  const isBullishEngulfing: boolean = isPreviousBearish && isCurrentBullish && currCandle.open <= prevCandle.close && currCandle.close >= prevCandle.open;
-  const isBearishEngulfing: boolean = isPreviousBullish && isCurrentBearish && currCandle.open >= prevCandle.close && currCandle.close <= prevCandle.open;
+  const isBullishEngulfing: boolean = prevBearish && currBullish && currCandle.open < prevCandle.close && currCandle.close > prevCandle.open;
+
+  const isBearishEngulfing: boolean = prevBullish && currBearish && currCandle.open > prevCandle.close && currCandle.close < prevCandle.open;
 
   if (isBullishEngulfing) return SIGNAL_DIRECTION.BULLISH;
   if (isBearishEngulfing) return SIGNAL_DIRECTION.BEARISH;
 
-  return null;
+  return SIGNAL_DIRECTION.NONE;
 };
