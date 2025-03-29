e1aa0a7ea23e62527cdec114d5dfe89f22be3693
focus1691 <mrjoshuaali@yahoo.co.uk>
2025-03-29 02:19:11 +0000

    Marubozu

--- src/lib/candlePatterns/doji.ts
+++ src/lib/candlePatterns/doji.ts
@@ -1,6 +1,5 @@
 import { SIGNAL_DIRECTION } from '../../constants/signals';
 import { ICandle } from '../../types';
-import { IDojiConfig } from '../../types/candlestickPatterns';
 
 /**
  * Detects a Doji candlestick pattern, indicating market indecision.
@@ -9,7 +8,8 @@ import { IDojiConfig } from '../../types/candlestickPatterns';
  * relative to the overall range (high - low).
  *
  * @param candle - The candle to evaluate.
- * @param config - Optional configuration to specify the Doji detection threshold.
+ * @param threshold - Optional threshold for the maximum body-to-range ratio to qualify as a Doji.
+ * @default 0.1 (10% of the total candle range)
  * @returns A SIGNAL_DIRECTION:
  *          - `BIDIRECTIONAL` if a Doji is detected.
  *          - `NONE` otherwise.
@@ -18,12 +18,11 @@ import { IDojiConfig } from '../../types/candlestickPatterns';
  * ```typescript
  * import * as ta from 'chart-patterns';
  *
- * const signal = ta.CandlestickPatterns.getDojiPatternDirection(candle, { threshold: 0.05 });
+ * const signal = ta.CandlestickPatterns.getDojiPatternDirection(candle, 0.05);
  * ```
  */
-export const getDojiPatternDirection = (candle: ICandle, config: IDojiConfig = {}): SIGNAL_DIRECTION => {
+export const getDojiPatternDirection = (candle: ICandle, threshold = 0.1): SIGNAL_DIRECTION => {
   const { open, close, high, low } = candle;
-  const threshold = config.threshold ?? 0.1;
 
   const bodySize = Math.abs(close - open);
   const range = high - low;
--- src/lib/candlePatterns/index.ts
+++ src/lib/candlePatterns/index.ts
@@ -2,3 +2,4 @@ export * from './engulfing';
 export * from './excess';
 export * from './doji';
 export * from './morningEveningStar';
+export * from './marubozu';
